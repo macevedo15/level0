c187c7401a6bc41c4932524ce4c36778e4ca514a
macevedo15 <31461429+macevedo15@users.noreply.github.com>
2017-08-29 19:22:28 -0700

    Add files via upload

---
+++ src/day4/RobotTreasureHunt.java
@@ -0,0 +1,114 @@
+package day4;
+
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import javax.swing.JOptionPane;
+
+//import org.teachingextensions.logo.ImageBackground;
+//import org.teachingextensions.logo.Paintable;
+//import org.teachingextensions.logo.robot;
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotTreasureHunt implements KeyEventDispatcher{
+
+	// 1. Create a new mini robot (type "mini" inside the parentheses)
+	Robot r2d2 = new Robot("mini");
+	private void goUp() throws InterruptedException {
+		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(0);
+		r2d2.microMove(10);
+	}
+
+	private void goDown() throws InterruptedException{
+		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(180);
+		r2d2.microMove(10);
+	}
+
+	private void turnLeft() throws InterruptedException{
+		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(270);
+		r2d2.microMove(10);
+	}
+
+	private void turnRight() throws InterruptedException{
+		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(90);
+		r2d2.microMove(10);
+	}
+
+	private void spaceBarWasPressed() {
+
+		// 5. Using the getX() and getY() methods, print out the x and y location of your robot
+		r2d2.getX();
+		r2d2.getY();
+		// 6. If robot is at same location as the little girl,
+		// make a pop-up tell the robot where to go next
+		
+		// 7. Give the user subsequent clues at different locations on the image
+		// (pirate robot, swamp, parrots, etc.)
+		
+		// 8.  If the tortoise is in the final location, call the treasureFound() method
+		
+	}
+
+	private void go() {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+		Robot.setWindowImage("day4/treasure_hunt.jpg");
+	
+		JOptionPane.showMessageDialog(null, "Ask the whale for help with your quest. Press the space bar to ask.");
+
+	}
+
+	public boolean dispatchKeyEvent(KeyEvent e) {
+	
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				try {
+					turnRight();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+				try {
+					turnLeft();
+				} catch (InterruptedException e2) {
+					e2.printStackTrace();
+				}
+			else if (e.getKeyCode() == 38)
+				try {
+					goUp();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+				try {
+					goDown();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			else if (e.getKeyCode() == KeyEvent.VK_SPACE)
+				spaceBarWasPressed();
+		}
+		return false;
+	}
+	
+	static void treasureFound() {
+		try {
+			URI uri = new URI("https://www.youtube.com/watch?v=G0aIg4N6aro");
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void main (String[] args) throws MalformedURLException {
+		new RobotTreasureHunt().go();
+	}
+}
